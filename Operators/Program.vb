6b62456cfa1b134b37e93bfe532014376ff951ea
rubenhortas <rubenhortas@gmail.com>
2022-03-26 20:41:47 +0100

    Added ternary operator example

---
+++ Operators/Program.vb
@@ -0,0 +1,10 @@
+Module Program
+    Sub Main()
+        ' Ternary operator
+        Dim isAnExample As Boolean = True
+        Dim exampleMessage = If(isAnExample = True, "This is an example", "This is NOT an example")
+
+        Console.WriteLine(exampleMessage)
+
+    End Sub
+End Module
