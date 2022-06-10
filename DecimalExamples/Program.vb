801a36bfef3cc070bb69fb7945340d3e011ad586
rubenhortas <rubenhortas@gmail.com>
2022-06-10 08:40:25 +0200

    Added examples

---
+++ DecimalExamples/Program.vb
@@ -0,0 +1,11 @@
+Module Program
+    Sub Main()
+        ' Convert String to Decimal
+        StringToDecimal.Parse("string1")
+        StringToDecimal.Parse(1)
+        StringToDecimal.TryParse("string2")
+        StringToDecimal.TryParse(2)
+
+        Console.ReadLine()
+    End Sub
+End Module
---
+++ DecimalExamples/StringToDecimal.vb
@@ -0,0 +1,24 @@
+﻿Public Class StringToDecimal
+    Public Shared Sub Parse(value As Object)
+        Dim d As Decimal
+
+        Try
+            d = Decimal.Parse(value)
+
+            Console.WriteLine($"d: {d}")
+        Catch ex As Exception
+            Console.WriteLine($"Unable to convert: {value}")
+        End Try
+    End Sub
+
+    Public Shared Sub TryParse(value As Object)
+        Dim d As Decimal
+
+        If Decimal.TryParse(value, d) Then
+            Console.WriteLine($"d: {d}")
+        Else
+            Console.WriteLine($"Unable to convert: {value}")
+        End If
+    End Sub
+
+End Class
