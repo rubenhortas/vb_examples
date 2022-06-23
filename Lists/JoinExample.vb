e7b9805ed08e621183283dfa7003572d1b5663f8
rubenhortas <rubenhortas@gmail.com>
2022-06-23 19:57:38 +0200

    Improved comment

--- Lists/JoinExample.vb
+++ Lists/JoinExample.vb
@@ -3,7 +3,7 @@
         Dim lstJoined As String
         Dim lst As New List(Of String) From {"a", "b", "c"}
 
-        ' Framework 3.5 with linq
+        ' Framework 3.5
         ' lst = String.Join(" ", lst.Cast(Of String))
 
         'Framework >=4
