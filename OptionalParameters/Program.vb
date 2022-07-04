beeaa63ee630c23cf6306ee879bcd0bc5622a973
rubenhortas <rubenhortas@gmail.com>
2022-07-04 20:59:09 +0200

    Added new example

---
+++ OptionalParameters/Program.vb
@@ -0,0 +1,14 @@
+Module Program
+    Sub Main()
+        RunMethod("Hello")
+        RunMethod("Hello", "world")
+    End Sub
+
+    Private Sub RunMethod(ByVal arg1 As String, Optional ByVal optionalArg As String = Nothing)
+        If optionalArg Is Nothing Then
+            Console.WriteLine($"{arg1}!")
+        Else
+            Console.WriteLine($"{arg1} {optionalArg}!")
+        End If
+    End Sub
+End Module
