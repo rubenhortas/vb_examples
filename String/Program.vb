6c1b562195baea8682995030ec5eb10dd78bfda8
rubenhortas <rubenhortas@gmail.com>
2022-07-06 21:42:49 +0200

    Improved

--- String/Program.vb
+++ String/Program.vb
@@ -2,6 +2,7 @@ Module Program
 
     Private Const OneTwoThree As String = "one, two, three"
     Private Const Numbers As String = "1234513370000"
+    Private Const Leet As String = "1337"
     Private Const HelloWorld As String = "Hello World!!"
     Private Const Nothing1 As String = Nothing
     Private Const Nothing2 As String = Nothing
@@ -28,20 +29,18 @@ Module Program
     Private Sub Split()
         Dim splittedString As String() = OneTwoThree.Split()
 
-        Console.WriteLine("Split:")
+        Console.WriteLine($"Splitting {OneTwoThree}:")
 
         For Each s As String In splittedString
-            Console.WriteLine(s)
+            Console.WriteLine($"{vbTab}s")
         Next
 
         Console.WriteLine()
     End Sub
 
     Private Sub Contains()
-        Console.WriteLine("Contains:")
-
-        If Numbers.Contains("1337") Then
-            Console.WriteLine("Y0u'r3 1337!")
+        If Numbers.Contains(Leet) Then
+            Console.WriteLine($"{Numbers} contains {Leet}. Y0u'r3 1337!")
         End If
 
         Console.WriteLine()
@@ -50,22 +49,19 @@ Module Program
     Private Sub Remove()
         Dim cleanHelloWorld As String = HelloWorld.Remove(HelloWorld.Length - 1)
 
-        Console.WriteLine("Remove:")
-        Console.WriteLine(cleanHelloWorld) ' Hello World!
+        Console.WriteLine($"Removing ""!"" from ""{HelloWorld}"": {cleanHelloWorld}") ' Hello world!
         Console.WriteLine()
     End Sub
 
     Private Sub Compare()
-        Console.WriteLine("Compare Nothing:")
-        Console.WriteLine($"String.Compare(Nothing, Nothing): {String.Compare(Nothing1, Nothing2)}") '0
+        Console.WriteLine($"String.Compare(Nothing1, Nothing2): {String.Compare(Nothing1, Nothing2)}") '0
+        Console.WriteLine($"Nothing1 = Nothing2: {Nothing1 = Nothing2}") 'True
         Console.WriteLine()
 
-        Console.WriteLine("Compare Nothing and String.Empty:")
         Console.WriteLine($"String.Compare(Nothing, String.Empty): {String.Compare(Nothing, String.Empty)}") '-1
         Console.WriteLine($"Nothing = String.Empty: {Nothing = String.Empty}") 'True
         Console.WriteLine()
 
-        Console.WriteLine("Compare two equal strings:")
         Console.WriteLine($"String.Compare({Abc1}, {Abc2}): {String.Compare(Abc1, Abc2)}") '0
         Console.WriteLine($"{Abc1} = {Abc2}: {Abc1 = Abc2}") 'True
         Console.WriteLine()
