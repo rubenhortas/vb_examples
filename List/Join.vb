e9dca3475d2dc8a04a40a29d0af69464dfd25537
rubenhortas <rubenhortas@gmail.com>
2022-07-06 21:33:52 +0200

    Improved

--- List/Join.vb
+++ List/Join.vb
@@ -1,16 +1,13 @@
 ﻿Public Class Join
 
     Public Shared Sub JoinLists()
-        Dim lstJoined As String
         Dim lst As New List(Of String) From {"a", "b", "c"}
 
         ' Framework 3.5
-        ' lst = String.Join(" ", lst.Cast(Of String))
+        ' String.Join(" ", lst.Cast(Of String))
 
         'Framework >=4
-        lstJoined = String.Join(",", lst)
-
-        Console.WriteLine($"Joined list as String: {lstJoined}")
+        Console.WriteLine($"Joined list as String: { String.Join(",", lst)}")
         Console.WriteLine()
     End Sub
 
