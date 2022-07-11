a6831fd4c88dedaa7317a2352ca513486a07cacf
rubenhortas <rubenhortas@gmail.com>
2022-07-11 20:40:51 +0200

    Fixed naming conventions

--- String/Program.vb
+++ String/Program.vb
@@ -1,13 +1,13 @@
 Module Program
 
-    Private Const OneTwoThree As String = "one, two, three"
-    Private Const Numbers As String = "1234513370000"
-    Private Const Leet As String = "1337"
-    Private Const HelloWorld As String = "Hello World!!"
-    Private Const Nothing1 As String = Nothing
-    Private Const Nothing2 As String = Nothing
-    Private Const Abc1 As String = "abc"
-    Private Const Abc2 As String = "abc"
+    Private Const oneTwoThree As String = "one, two, three"
+    Private Const numbers As String = "1234513370000"
+    Private Const leet As String = "1337"
+    Private Const helloWorld As String = "Hello World!!"
+    Private Const nothing1 As String = Nothing
+    Private Const nothing2 As String = Nothing
+    Private Const abc1 As String = "abc"
+    Private Const abc2 As String = "abc"
 
     Sub Main()
         Join()
@@ -27,9 +27,9 @@ Module Program
     End Sub
 
     Private Sub Split()
-        Dim splittedString As String() = OneTwoThree.Split()
+        Dim splittedString As String() = oneTwoThree.Split()
 
-        Console.WriteLine($"Splitting {OneTwoThree}:")
+        Console.WriteLine($"Splitting {oneTwoThree}:")
 
         For Each s As String In splittedString
             Console.WriteLine($"{vbTab}{s}")
@@ -39,31 +39,31 @@ Module Program
     End Sub
 
     Private Sub Contains()
-        If Numbers.Contains(Leet) Then
-            Console.WriteLine($"{Numbers} contains {Leet}. Y0u'r3 1337!")
+        If numbers.Contains(leet) Then
+            Console.WriteLine($"{numbers} contains {leet}. Y0u'r3 1337!")
         End If
 
         Console.WriteLine()
     End Sub
 
     Private Sub Remove()
-        Dim cleanHelloWorld As String = HelloWorld.Remove(HelloWorld.Length - 1)
+        Dim cleanHelloWorld As String = helloWorld.Remove(helloWorld.Length - 1)
 
-        Console.WriteLine($"Removing ""!"" from ""{HelloWorld}"": {cleanHelloWorld}") ' Hello world!
+        Console.WriteLine($"Removing ""!"" from ""{helloWorld}"": {cleanHelloWorld}") ' Hello world!
         Console.WriteLine()
     End Sub
 
     Private Sub Compare()
-        Console.WriteLine($"String.Compare(Nothing1, Nothing2): {String.Compare(Nothing1, Nothing2)}") '0
-        Console.WriteLine($"Nothing1 = Nothing2: {Nothing1 = Nothing2}") 'True
+        Console.WriteLine($"String.Compare(Nothing1, Nothing2): {String.Compare(nothing1, nothing2)}") ' 0
+        Console.WriteLine($"Nothing1 = Nothing2: {nothing1 = nothing2}") ' True
         Console.WriteLine()
 
-        Console.WriteLine($"String.Compare(Nothing, String.Empty): {String.Compare(Nothing, String.Empty)}") '-1
-        Console.WriteLine($"Nothing = String.Empty: {Nothing = String.Empty}") 'True
+        Console.WriteLine($"String.Compare(Nothing, String.Empty): {String.Compare(Nothing, String.Empty)}") ' -1
+        Console.WriteLine($"Nothing = String.Empty: {Nothing = String.Empty}") ' True
         Console.WriteLine()
 
-        Console.WriteLine($"String.Compare({Abc1}, {Abc2}): {String.Compare(Abc1, Abc2)}") '0
-        Console.WriteLine($"{Abc1} = {Abc2}: {Abc1 = Abc2}") 'True
+        Console.WriteLine($"String.Compare({abc1}, {abc2}): {String.Compare(abc1, abc2)}") ' 0
+        Console.WriteLine($"{abc1} = {abc2}: {abc1 = abc2}") ' True
         Console.WriteLine()
     End Sub
 
