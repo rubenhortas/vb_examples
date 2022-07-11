ffddbbbc61310c0239a691a8cc7cb70c18f060ec
rubenhortas <rubenhortas@gmail.com>
2022-07-11 20:41:37 +0200

    Fixed naming conventions

--- TernaryOperator/Program.vb
+++ TernaryOperator/Program.vb
@@ -1,7 +1,7 @@
 Module Program
 
     Sub Main()
-        ' Ternary operator
+        ' Ternary operator.
         Dim isAnExample As Boolean = True
         Dim exampleMessage = If(isAnExample = True, "This is an example", "This is NOT an example")
 
