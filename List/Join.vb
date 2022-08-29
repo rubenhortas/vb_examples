c97dc655f46b9f2df52a93928ff7c7c3280a3924
rubenhortas <rubenhortas@gmail.com>
2022-08-29 17:59:15 +0200

    Added new examples

--- List/Join.vb
+++ List/Join.vb
@@ -2,13 +2,20 @@
 
     Public Shared Sub JoinLists()
         Dim lst As New List(Of String) From {"a", "b", "c"}
+        Dim emptyList As List(Of String) = New List(Of String)
 
         ' Framework 3.5
         ' String.Join(" ", lst.Cast(Of String))
 
         ' Framework >=4
         Console.WriteLine($"Joined list as String: { String.Join(",", lst)}")
+        ' Result: a, b, c.
         Console.WriteLine()
+
+        ' Join an empty list will result on a empty string
+        Console.WriteLine($"Joined emtyList: { String.Join(",", emptyList)}")
+
+        ' Try to join a null list will result on an exception
     End Sub
 
 End Class
